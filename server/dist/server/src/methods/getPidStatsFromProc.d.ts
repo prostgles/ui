1844b985fbc21dd648fca33c345aee916ad168c7
prostgles <prostgles@protonmail.com>
2024-07-07 13:59:26 +0300

    refresh gitignore

--- server/dist/server/src/methods/getPidStatsFromProc.d.ts
+++
@@ -1,4 +0,0 @@
-import type { DB } from "prostgles-server/dist/Prostgles";
-import { type ServerLoadStats } from "./getPidStats";
-export declare const getPidStatsFromProc: (db: DB, connId: string) => Promise<ServerLoadStats>;
-//# sourceMappingURL=getPidStatsFromProc.d.ts.map
