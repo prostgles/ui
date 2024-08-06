1fb69a12cd12d20494774b7aeb8eec488c1cc2f8
prostgles <prostgles@protonmail.com>
2024-08-06 17:47:34 +0300

    update electron artifacts name

--- electron/e2e-electron/electron.spec.tsx
+++ electron/e2e-electron/electron.spec.tsx
@@ -41,7 +41,7 @@ test('renders the first page', async () => {
 
   const screenshot = async (name?: string) => {
     if(!page) return;
-    await page.screenshot({ path: `./dist/s_${name ?? (new Date()).toISOString()}.png` });
+    await page.screenshot({ path: `./dist/s-${name ?? (new Date()).toISOString().replaceAll(":", "")}.png` });
   }
 
   await page.waitForTimeout(4000);
