9d869ce673b3af5266a07880792316b7daf53af6
prostgles <prostgles@protonmail.com>
2024-09-11 23:36:01 +0300

    fix test

--- electron/e2e-electron/electron.spec.tsx
+++ electron/e2e-electron/electron.spec.tsx
@@ -34,9 +34,7 @@ test.afterAll(async () => {
   await electronApp?.close()
 })
 
-// let page: Page | undefined;
-
-test.setTimeout(60e3);
+test.setTimeout(120e3);
 
 test('renders the first page', async () => {
   if(!electronApp) return;
