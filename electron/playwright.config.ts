71891d305fb046a8e11f42481075e432fa9b5149
prostgles <prostgles@protonmail.com>
2024-09-11 23:11:52 +0300

    fix test

--- electron/playwright.config.ts
+++ electron/playwright.config.ts
@@ -7,9 +7,11 @@ export default defineConfig({
   maxFailures: 0,
   workers: 1,
   reporter: 'html',
+  retries: 0,
   use: {
+    /** https://github.com/microsoft/playwright/issues/27048 */
     trace: "off",
-    video: "on",
+    video: "off",
     testIdAttribute: "data-command",
   }
 });
