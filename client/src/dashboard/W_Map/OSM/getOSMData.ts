53ce70ea7558edeaec9cd586ca3dbd84a016fc31
prostgles <prostgles@protonmail.com>
2024-10-13 22:39:31 +0300

    fix osm polygon bug

--- client/src/dashboard/W_Map/OSM/getOSMData.ts
+++ client/src/dashboard/W_Map/OSM/getOSMData.ts
@@ -1,5 +1,6 @@
 import { isDefined } from "../../../utils";
-import type { GeoJSONFeature, Polygon } from "../../Map/DeckGLMap";
+import type { GeoJSONFeature } from "../../Map/DeckGLMap";
+import { osmRelationToGeoJSON } from "./osmToGeoJSON";
 
 const OVERPASS_URL = "https://overpass-api.de/api/interpreter";
 
@@ -11,41 +12,42 @@ type OSMElementBase = {
   tags: { [key: string]: string | undefined };
 }
 
-type OSMNode = OSMElementBase & {
+export type OSMNode = OSMElementBase & {
   type: "node";
   lat: number;
   lon: number;
 }
 
-type OSMWay = Omit<OSMElementBase, "type"> & {
+export type OSMWay = Omit<OSMElementBase, "type"> & {
   type: "way";
   nodes: number[]; // Array of node IDs
 }
 
-type OSMRelationMemberWay = {
+export type OSMRelationMemberWay = {
   ref: number; // Reference ID of the member element
   type: "way";
   role: "outer" | "inner";
 }
-type OSMRelationMemberNode = {
+export type OSMRelationMemberNode = {
   ref: number; // Reference ID of the member element
   type: "node";
   role: "admin_centre" | "label";
 }
-type OSMRelationMember = OSMRelationMemberWay | OSMRelationMemberNode;
+export type OSMRelationMember = OSMRelationMemberWay | OSMRelationMemberNode;
 
-type OSMRelation = OSMElementBase & {
+export type OSMRelation = OSMElementBase & {
   type: "relation";
   members: OSMRelationMember[];
 }
 
-type OSMElement = OSMNode | OSMWay | OSMRelation;
+export type OSMElement = OSMNode | OSMWay | OSMRelation;
 
 const cachedFeatures: Map<number, GeoJSONFeature> = new Map();
 export const getOSMData = async (query: string, bbox: string) => {  
   const response = await fetch(OVERPASS_URL, {
     method: "POST",
     body: query.replace(/\${bbox}/g, bbox),
+    cache: "default",
   });
   const data: {
     elements: OSMElement[];
@@ -55,7 +57,7 @@ export const getOSMData = async (query: string, bbox: string) => {
   const features = await getOSMDataAsGeoJson({
     elements: nonCached
   });
-  console.log(data.elements)
+  console.log(data.elements, features)
   return { 
     data, 
     features: [...cache.filter(isDefined), ...features], 
@@ -63,55 +65,72 @@ export const getOSMData = async (query: string, bbox: string) => {
 }
 
 const cachedElements: Map<number, OSMElement> = new Map();
-const fetchElements = async <T extends "node" | "way">(type: T, ids: number[]): Promise<(T extends "node"? OSMNode : OSMWay)[]> => {
-  if(!ids.length) return [];
+const fetchElements = async <T extends "node" | "way">(type: T, ids: number[]): Promise<Map<number, T extends "node"? OSMNode : OSMWay>> => {
+  if(!ids.length) return new Map();
   const limit = 10000;
   let batchNodeIds: number[];
   let step = 0;
   const nonCachedIds = ids.filter(id => !cachedElements.has(id));
   let elements: any[] = ids.map(id => cachedElements.get(id)).filter(isDefined);
   do {
-    batchNodeIds = nonCachedIds.splice(step * limit, limit);
+    batchNodeIds = nonCachedIds.splice(0, limit);
     step++;
     if(!batchNodeIds.length) break;
     const nodeBatch = await fetch(OVERPASS_URL, {
       method: "POST",
-      body: `[out:json];${type}(id:${batchNodeIds.join(",")});out;`,
+      body: `[out:json];${type}(id:${batchNodeIds.join(",")});out ${limit};`,
     }).then(res => res.json()).then(res => res.elements);
-    elements = [...elements, ...nodeBatch];
+    elements = [
+      ...elements, 
+      ...nodeBatch
+    ];
     nodeBatch.forEach((node: any) => {
       cachedElements.set(node.id, node);
     });
   } while (batchNodeIds.length);
 
-  return elements;
+  const map = new Map<number, any>();
+  elements.forEach((node: any) => {
+    map.set(node.id, node);
+  });
+  return map;
 }
 
-export const getOSMDataAsGeoJson = async (responseData: { elements: OSMElement[]; }) => {
+const wayToLineString = (way: OSMWay, nodes: Map<number, OSMNode>): GeoJSONFeature["geometry"] => {
+  const coordinates: [number, number][] = way.nodes.map((nodeId: number) => {
+    const node = nodes.get(nodeId);
+    return node ? [node.lon, node.lat] satisfies [number, number] : undefined;
+  }).filter(isDefined);
+  
+  return  {
+    type: "LineString",
+    coordinates,
+  } 
+
+}
+
+export const getOSMDataAsGeoJson = async (responseData: { elements: OSMElement[]; }): Promise<GeoJSONFeature[]> => {
   const unique = <T,>(arr: T[]) => Array.from(new Set(arr));
-  const wayIds = unique(responseData.elements.flatMap((e) => e.type === "relation"? e.members.flatMap(m => m.type === "way"? m.ref : undefined).filter(isDefined) : undefined).filter(isDefined));
+  const wayIds = unique(
+    responseData.elements
+      .flatMap((e) => 
+        e.type === "relation"? e.members.flatMap(m => m.type === "way"? m.ref : undefined).filter(isDefined) : undefined
+      ).filter(isDefined)
+    );
   const _nodeIds = unique(responseData.elements.flatMap((e) => {
+    if(e.type === "node") return [e.id];
     if(e.type === "way") return e.nodes;
     // if(e.type === "relation") return e.members.map(member => member.ref);
     return [];
-  }));
-  // const ways: OSMWay[] = !wayIds.length? [] : await fetch(OVERPASS_URL, {
-  //   method: "POST",
-  //   body: `[out:json];way(id:${wayIds.join(",")});out;`,
-  // })
-  // .then(res => res.json())
-  // .then(res => res.elements);
+  })); 
   const ways = await fetchElements("way", wayIds);
-  const wayNodeIds = unique(ways.flatMap(w => w.nodes));
+  const wayNodeIds = unique(ways.values().toArray().flatMap(w => w.nodes));
   const nodeIds = unique([ ..._nodeIds, ...wayNodeIds]);
-  
-  // const nodes: OSMNode[] = !nodeIds.length? [] : await fetch(OVERPASS_URL, {
-  //   method: "POST",
-  //   body: `[out:json];node(id:${nodeIds.join(",")});out;`,
-  // }).then(res => res.json()).then(res => res.elements);
   const nodes = await fetchElements("node", nodeIds);
   
-  const features: GeoJSONFeature[] = responseData.elements.map((element) => {
+  const node = await fetchElements("node", [36935764]);
+  console.log(node.values().toArray());
+  const features: GeoJSONFeature[] = responseData.elements.flatMap((element) => {
     const commonProps: Pick<GeoJSONFeature, "id" | "properties" | "type"> = {
       id: element.id,
       type: "Feature",
@@ -128,90 +147,57 @@ export const getOSMDataAsGeoJson = async (responseData: { elements: OSMElement[]
           coordinates: [element.lon, element.lat],
         },
       } satisfies GeoJSONFeature;
-    } else if (element.type === "way") {
-      const coordinates: [number, number][] = element.nodes.map((nodeId: number) => {
-        const node = nodes.find((el: any) => el.id === nodeId);
-        return node ? [node.lon, node.lat] satisfies [number, number] : undefined;
-      }).filter(isDefined);
-      
+    } else if (element.type === "way") {  
       return {
         ...commonProps,
-        geometry: {
-          type: "LineString",
-          coordinates,
-        },
+        geometry: wayToLineString(element, nodes),
       } satisfies GeoJSONFeature;
+
+    /*
+      A relation can be a multipolygon or a route, node, etc.
+    */
     } else if ((element as any).type === "relation") {
-      // Assuming multipolygon for simplicity
+      // return ways.values().toArray().map(way => ({
+      //   ...commonProps,
+      //   geometry: wayToLineString(way, nodes),
+      // }))
 
-      // Convert ways to GeoJSON coordinates
-      const convertWayToCoordinates = (m: OSMRelationMemberWay) => {
-        const way = ways.find((el) => el.id === m.ref);
-        if(!way) {
-          console.warn(`Way with ID ${m.ref} not found`);
-          return [];
-        }
-        const polygon: [number,number][] = way.nodes.map(nodeId => {
-          const node = nodes.find((el) => el.id === nodeId);
-          if(!node) return;
-          const point: [number,number] = [node.lon, node.lat];
-          return point;
-        }).filter(isDefined);
-
-        return polygon;
-      };
-
-      const outerWays = element.members.filter((m): m is OSMRelationMemberWay => m.type === "way" && m.role === "outer");
-      const innerWays = element.members.filter((m): m is OSMRelationMemberWay => m.type === "way" && m.role === "inner");
-     
-      const coordinates: [number,number][][] = [];
-
-
-      // Process outer and inner ways
-      // element.members.forEach(m => {
-      //   if(m.type !== "way") return;
-      //   const way = ways;
-      //   const role = m.role;
-      //   if (role === 'outer') {
-      //     // For each outer way, we start a new polygon
-      //     const polygon: [number, number][][] = [convertWayToCoordinates(m.)];
-
-      //     // Find matching inner ways to this outer way (if applicable)
-      //     // Note: This example does not implement the logic to check if an inner way is within an outer way.
-      //     // In a real scenario, such spatial checks are necessary to correctly assign inner ways to their respective outer ways.
-      //     const innerWays = relation.ways.filter(innerRelationWay => innerRelationWay.role === 'inner').map(innerRelationWay => innerRelationWay.way);
-
-      //     innerWays.forEach(innerWay => {
-      //       // Add inner ways as holes in the polygon
-      //       polygon.push(convertWayToCoordinates(innerWay));
-      //     });
-
-      //     coordinates.push(polygon);
+      return osmRelationToGeoJSON(element, nodes, ways);
+      // // Convert ways to GeoJSON coordinates
+      // const convertWayToCoordinates = (m: OSMRelationMemberWay) => {
+      //   const way = ways.find((el) => el.id === m.ref);
+      //   if(!way) {
+      //     console.warn(`Way with ID ${m.ref} not found`);
+      //     return undefined;
       //   }
-      // });
+      //   if(!way.nodes.length) {
+      //     console.warn(`Way with ID ${m.ref} has no nodes`);
+      //     return undefined;
+      //   }
+      //   const polygon: [number,number][] = way.nodes.map(nodeId => {
+      //     const node = nodes.find((el) => el.id === nodeId);
+      //     if(!node) return;
+      //     const point: [number,number] = [node.lon, node.lat];
+      //     return point;
+      //   }).filter(isDefined);
 
-      // // Assuming a simple structure where each outer way is a separate polygon
-      // // and inner ways are holes in these polygons. This logic will need to be
-      // // more complex for real-world data handling.
-      // outerWays.forEach(ow => {
-      //   const outerCoordinates = [convertWayToCoordinates(ow)];
-      //   innerWays.forEach(iw => {
-      //     // Here you would typically check if the inner way is within the outer way
-      //     // For simplicity, we're just adding all inners as holes to each outer
-      //     outerCoordinates.push(convertWayToCoordinates(iw));
-      //   });
-      //   coordinates.push(outerCoordinates);
-      // });
+      //   return polygon;
+      // };
 
-      const outerPolygon: Polygon["coordinates"] = outerWays.map(ow => convertWayToCoordinates(ow));
-      // const innerPolygons
-      return {
-        ...commonProps,
-        geometry: {
-          type: "Polygon",
-          coordinates: outerPolygon,
-        },
-      } satisfies GeoJSONFeature;
+      // const outerWays = element.members.filter((m): m is OSMRelationMemberWay => m.type === "way" && m.role === "outer");
+      // const innerWays = element.members.filter((m): m is OSMRelationMemberWay => m.type === "way" && m.role === "inner");
+     
+      // const coordinates: [number,number][][] = [];
+
+      // const outerPolygon: Polygon["coordinates"] = outerWays.map(ow => convertWayToCoordinates(ow)).filter(isDefined);
+      // // const innerPolygons
+      // return {
+      //   ...commonProps,
+      //   geometry: {
+      //     type: "Polygon",
+      //     coordinates: outerPolygon,
+      //   },
+      // } satisfies GeoJSONFeature;
     }
     return undefined;
   }).filter(isDefined);
