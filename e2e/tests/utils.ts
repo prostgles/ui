5977fad6e20964804c4afb6a956c055cb8c38526
prostgles <prostgles@protonmail.com>
2024-10-25 17:45:23 +0300

    fix llm rate limit

--- e2e/tests/utils.ts
+++ e2e/tests/utils.ts
@@ -509,16 +509,13 @@ export const createAccessRule = async (page: PageWIds, userType: "default" | "pu
 }
 
 export const enableAskLLM = async (page: PageWIds, maxRequestsPerDay: number, credsProvided=false) => {
-  if(credsProvided){
-    await page.getByTestId("AskLLMAccessControl").click();
-    await page.getByTestId("AskLLMAccessControl.AllowAll").click();
-    await page.getByTestId("Popup.close").click();
-    return
-  }
   await page.getByTestId("AskLLMAccessControl").click();
-  await fillSmartFormAndInsert(page, "llm_credentials", { endpoint: "http://localhost:3004/mocked-llm" });
-  await page.waitForTimeout(1e3);
+  if(!credsProvided){
+    await fillSmartFormAndInsert(page, "llm_credentials", { endpoint: "http://localhost:3004/mocked-llm" });
+    await page.waitForTimeout(1e3);
+  }
   await page.getByTestId("AskLLMAccessControl.AllowAll").click();
+  await page.waitForTimeout(1e3);
   if(maxRequestsPerDay){
     await page.getByTestId("AskLLMAccessControl.llm_daily_limit").fill(maxRequestsPerDay.toString());
   }
