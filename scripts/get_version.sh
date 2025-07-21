52a5e3cd776554642e09241e2d5ed0bd75e53b92
prostgles <prostgles@protonmail.com>
2025-07-21 13:12:07 +0300

    add release file

--- scripts/get_version.sh
+++ scripts/get_version.sh
@@ -2,4 +2,10 @@
 
 file_path="${1:-./package.json}"
 version=$(grep -m1 '"version":' "$file_path" | cut -d '"' -f4)
+
+if [ -z "$version" ]; then
+  echo "Version not found in $file_path"
+  exit 1
+fi
+
 echo "$version"
