4e91ecd7686642b0070451e47c6821a33c19d958
prostgles <prostgles@protonmail.com>
2025-11-24 17:05:08 +0000

    wip fix command palette search

--- client/src/app/CommandPalette/CommandPaletteSearch.spec.tsx
+++ client/src/app/CommandPalette/CommandPaletteSearch.spec.tsx
@@ -1,27 +1,27 @@
 import { getItemSearchRank } from "@components/SearchList/searchMatchUtils/getItemSearchRank";
 import { flatUIDocs } from "../UIDocs";
+import { strict } from "assert";
+//@ts-ignore
+import { test } from "node:test";
 
-/** We expect every exactly typed title to show first */
-flatUIDocs.forEach(({ title: searchTerm }) => {
-  let lowestRank = { value: -Infinity, title: "" };
-  flatUIDocs.forEach(({ title, description, parentTitles }) => {
-    const rank = getItemSearchRank(
-      {
-        title,
-        subTitle: description,
-        level: parentTitles.length,
-      },
-      searchTerm,
-    );
+void test("Every exactly typed title to be shown first", async () => {
+  flatUIDocs.forEach(({ title: searchTerm }) => {
+    let lowestRank = { value: -Infinity, title: "" };
+    flatUIDocs.forEach(({ title, description, parentTitles }) => {
+      const rank = getItemSearchRank(
+        {
+          title,
+          subTitle: description,
+          level: parentTitles.length,
+        },
+        searchTerm,
+      );
 
-    if (rank < lowestRank.value) {
-      lowestRank = { value: rank, title };
-    }
+      if (rank < lowestRank.value) {
+        lowestRank = { value: rank, title };
+      }
+    });
+
+    strict.equal(searchTerm, lowestRank.title);
   });
-
-  if (lowestRank.title !== searchTerm) {
-    throw new Error(
-      `Search ranking failed for term "${searchTerm}". Expected top result to be "${searchTerm}", but got "${lowestRank.title}" with rank ${lowestRank.value}`,
-    );
-  }
 });
